b18baf0aa94ce012a45466edfcfede58999772ba
saiteja244 <saitejachauhan244@gmail.com>
2026-06-04 18:19:38 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -31,5 +31,5 @@ if __name__ == "__main__":
     app.run(
         debug=True,
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000))
+        port=int(os.environ.get("PORT", 10000))
     )
